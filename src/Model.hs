619c845a86946621ea7f912893f64cde181afdbf
Abel van Beek <a.vanbeek@students.uu.nl>
2017-10-19 20:38:32 +0200

    Data types and changed gamestate datatype

--- src/Model.hs
+++ src/Model.hs
@@ -2,17 +2,42 @@
 --   which represent the state of the game
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
-
-nO_SECS_BETWEEN_CYCLES :: Float
-nO_SECS_BETWEEN_CYCLES = 5
+-- Main game status data types
+data GameStatus = Menu 
+                | Play
+                | Pause 
+                | GameOver
 
 data GameState = GameState {
-                   infoToShow  :: InfoToShow
+                   gameStatus  :: GameStatus
                  , elapsedTime :: Float
                  }
 
+-- Level/Score related data types
+type Level = [Gameobject]
+
+data Score = Score Int
+data Lives = Lives Int
+
+-- Display data types
+data Velocity = Velocity { xvel :: Float, yvel :: Float }
+data Position = Position { xpos :: Int, ypos :: Int }
+
+data Size = Radius { r :: Float } | Rectangle { w :: Float, h :: Float }
+data Sprite = Picture
+type Animation = [Sprite]
+
+-- Game object related data types
+data Objectinfo = Color Velocity Position Sprite Size
+data Gameobject = Wall Objectinfo | Ball Objectinfo | Player PlayerInfo| Arrow Objectinfo
+
+-- Player related data types
+data IsShooting = True | False
+data PlayerInfo = PlayerInfo Objectinfo Score IsShooting Lives
+data Player = P1 PlayerInfo | P2 PlayerInfo
+
 initialState :: GameState
-initialState = GameState ShowNothing 0
+initialState = GameState Menu 0
+
+nO_SECS_BETWEEN_CYCLES :: Float
+nO_SECS_BETWEEN_CYCLES = 5
