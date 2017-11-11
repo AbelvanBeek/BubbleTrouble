25fdd1696809e3926645b9679b2b69d1488bac79
Abel van Beek <a.vanbeek@students.uu.nl>
2017-11-11 17:03:53 +0100

    xplos

--- src/InitialStates.hs
+++ src/InitialStates.hs
@@ -2,11 +2,12 @@ module InitialStates where
     
 import Model
 import Random
+import LoadPictures
 
 {- Initialize States -}
 
 initialMenu :: IO GameState
-initialMenu = return $ GameState Menu (Level undefined undefined undefined undefined undefined undefined undefined undefined) undefined
+initialMenu = return $ GameState Menu (Level undefined undefined undefined undefined undefined undefined undefined loadPictures) undefined
 
 initialPlay :: IO GameState
 initialPlay = do rndlvl <- randomPlayLevel
--- src/View.hs
+++ src/View.hs
@@ -3,6 +3,7 @@
 module View where
 
 import Graphics.Gloss
+import Graphics.Gloss.Juicy
 
 import Model
 import HighScores
@@ -14,10 +15,20 @@ view gstat = do gstate <- gstat
 
 viewPure :: GameState -> IO Picture
 viewPure gstate = case gstate of
-    GameState Menu lvl _       ->  return $ translate (-560) 0 $ color green (text ".:Bubble Trouble:.")
+    GameState Menu (Level _ _ _ _ _ _ _ pics) _ -> do x <- pics !! 8
+                                                      return $ pictures ((scale 4 4 x) : [(translate (-560) 0 $ color green (text ".:Bubble Trouble:."))])
     GameState Play lvl _       ->  do x <- sequence $ drawLevel lvl
                                       return (pictures x)
     GameState Pause lvl _      ->  do x <- sequence $ drawLevel lvl
-                                      return (pictures ((translate (-325) 0 $ color blue (text ".:Paused:.")) : x))
-    GameState GameOver lvl _   ->  do updateHighScores lvl
-                                      return $ translate (-350) 0 $ color black (text ".:GameOver:.")
+                                      return $ pictures (x ++ [translate (-325) 0 $ color blue (text ".:Paused:.")])
+    GameState GameOver lvl@(Level (Player (P1 (PlayerInfo _ p1score _ _))) _ (Player (P2 (PlayerInfo _ p2score _ _))) _ _ _ _ pics) _   ->  
+                                                          do x <- pics !! 8
+                                                             updateHighScores lvl
+                                                             return $ pictures ((scale 4 4 x) : 
+                                                                                (translate (-350) (100) $ color black (text ".:GameOver:.")) :
+                                                                                (scale 0.5 0.5 $ translate (-600) (-100) $ color black (text "Player 1")) :
+                                                                                (scale 0.5 0.5 $ translate (-410) (-350) $ color black (text (show p1score))) :                                                                                
+                                                                                (scale 0.5 0.5 $ translate (200) (-100) $ color black (text "Player 2")) :
+                                                                                (scale 0.5 0.5 $ translate (330) (-350) $ color black (text (show p2score))) :                                                                                
+                                                                                []
+                                                                               )
